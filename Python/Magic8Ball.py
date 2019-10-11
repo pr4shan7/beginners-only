06799badafe2c06d4e7bbe8853f45547261dc472
Mayank <msharma.onetriple9@gmail.com>
2019-10-11 22:27:27 +0530

    Magic8ball.py

---
+++ Python/Magic8Ball.py
@@ -0,0 +1,21 @@
+# Magic8-Ball
+import random                 #using random module to get random predictions
+def getpredict(answerNumber):
+    if answerNumber == 1:
+        return "It is certain."
+    elif answerNumber == 2:
+        return "My sources say no."
+    elif answerNumber == 3:
+        return "It is decidedly so."
+    elif answerNumber == 4:
+        return "Reply hazy try again."
+    elif answerNumber == 5:
+        return "Outlook not so good."
+    elif answerNumber == 6:
+        return "Concentrate and ask again."
+    elif answerNumber == 7:
+        return "My reply is no."
+    elif answerNumber == 8:
+        return "Ask again later."
+print(getpredict(random.randint(1, 8)))
+    
