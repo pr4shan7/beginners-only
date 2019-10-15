a37a3539e633ec9a114179b44f44b229e391e332
Devansh Thapa <devanshthapa1903@gmail.com>
2019-10-16 04:23:05 +0530

    Update base-converter.c
    added some comments and made changes to some comments

--- C/base-converter.c
+++ C/base-converter.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 
-int main(void)
+int main(void)//main function
 {
 	long long int n , m = 0, dec = 0, r = 1;
 	int x , y;
 	scanf("%lld %d %d", &n, &x, &y);		//input number n in base x to be converted to base y
 	dec = dec + ( n % 10);
-	for(int i = 1; n > 0; i++)
+	for(int i = 1; n > 0; i++)//First for loop 
 	{
 		r = r * x;
 		n = n / 10;
 		dec = dec + (r * (n % 10 ));
 	}
 	n = 1;
-	for(int i = 1; dec > 0; i++)
+	for(int i = 1; dec > 0; i++)//Second for loop
 	{
 		r = dec % y;
 		dec = dec / y;
