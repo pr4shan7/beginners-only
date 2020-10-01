a093ddded19a32a1f425b4fb0e43d6a52e56ea8a
NameerWaqas <nameerwaqas321@gmail.com>
2020-10-01 12:42:52 +0500

    Bug: Made a global userList constant Details: Since in the previous commit both the APIs used different data points.After adding a new user through POST request,a person will not be able to get all the users through GET requests.

--- JavaScript/Node.js/Express.js/Beginner API/app.js
+++ JavaScript/Node.js/Express.js/Beginner API/app.js
@@ -8,32 +8,25 @@ app.use(express.json());
 
 // cd into the Node.js\APIs\Express.js\Beginner folder, and run 'npm install', then 'npm start' to start the server!
 
-// Simple GET endpoint
-app.get('/users', (req, res) => {
-    const userList = [
-        {name: 'Mike', age: 23},
-        {name: 'Jim', age: 41},
-        {name: 'Terra', age: 19},
-        {name: 'Richard', age: 32},
-        {name: 'Alex', age: 56},
-    ];
+// Single data array for both Get and POST requests.
+const userList = [
+    {name: 'Mike', age: 23},
+    {name: 'Jim', age: 41},
+    {name: 'Terra', age: 19},
+    {name: 'Richard', age: 32},
+    {name: 'Alex', age: 56},
+];
 
+
+// Simple GET endpoint
+app.get('/users', (req, res) => {  
     res.send(userList);
     // GET localhost:3000/users = [{"name": "Mike", "age": 23}, {"name": "Jim", "age": 41},..]
 });
 
 // Simple POST endpoint
 app.post('/createUser', (req, res) => {
-    const userList = [
-        {name: 'Mike', age: 23},
-        {name: 'Jim', age: 41},
-        {name: 'Terra', age: 19},
-        {name: 'Richard', age: 32},
-        {name: 'Alex', age: 56},
-    ];
-
     userList.push({name: req.body.name, age: req.body.age})
-
     res.send(userList)
     // POST localhost:3000/createUser using {"name": "Bob", "age": 44} to add him to the list!
 });
